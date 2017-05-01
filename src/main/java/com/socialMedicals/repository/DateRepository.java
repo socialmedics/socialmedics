2764788f61200f85001e82c1b0d5e21af050848a
LAPTOP-1206GM4S\yonay <yonaycl@gmail.com>
2017-05-01 18:07:50 +0100

    new

--- src/main/java/com/socialMedicals/repository/DateRepository.java
+++ src/main/java/com/socialMedicals/repository/DateRepository.java
@@ -1,9 +1,8 @@
 package com.socialMedicals.repository;
 
-import com.socialMedicals.entity.Medics;
+import com.socialMedicals.entity.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.socialMedicals.entity.Date;
 
 import java.util.List;
 
